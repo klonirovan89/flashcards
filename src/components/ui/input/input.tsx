0d5dc684b693372e9dc1a76dc0d978bc2957e066
Alexsander Fisiuk <alexanderfisiuk@gmail.com>
2024-01-15 19:09:51 +0300

    + search

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -3,30 +3,46 @@ import {clsx} from 'clsx'
 
 import s from './input.module.scss'
 import {useState} from "react";
+import {EyeNoneIcon, EyeOpenIcon, MagnifyingGlassIcon} from "@radix-ui/react-icons";
 
 export type SuperInputProps = {
     placeholder?: string
     title?: string
     type?: 'default' | 'password' | 'search'
     errorMessage?: string
+    disabled?: boolean
 }
 
 export const SuperInput = (props: SuperInputProps) => {
-    const {placeholder, title, type = 'default', errorMessage} = props
-    const [isVisiblePassword, setIsVisiblePassword] = useState<boolean>(true)
+    const {placeholder, title, type = 'default', errorMessage, disabled = false} = props
+    const [isVisiblePassword, setIsVisiblePassword] = useState<boolean>(false)
 
     const finalInputType = isVisiblePassword ? 'default' : type
-    const inputClassName = clsx(s.input, s[errorMessage ? 'error' : ''])
+    const inputClassName = clsx(s.input, s[errorMessage ? 'error' : ''], s[type])
+
     const eyeClassName = clsx(s.eyeButton, s[isVisiblePassword ? 'opened' : 'closed'])
+    const eyeOnClick = () => setIsVisiblePassword(!isVisiblePassword)
 
     return (
         <div className={s.superInput}>
             <Typography variant={'body2'}>{title}</Typography>
+
             <div className={s.textField}>
-                <input className={inputClassName} placeholder={placeholder} type={finalInputType}/>
-              {type === 'password' ? <div className={eyeClassName} onClick={() => setIsVisiblePassword(!isVisiblePassword)}></div> : ''}
+
+                <input className={inputClassName} placeholder={placeholder} type={finalInputType} disabled={disabled}/>
+
+                {type === 'password' ?
+                    isVisiblePassword ? <EyeNoneIcon className={eyeClassName} onClick={eyeOnClick}/>
+                        : <EyeOpenIcon className={eyeClassName} onClick={eyeOnClick}/>
+                    : ''}
+
+                {type === 'search' ?
+                    <MagnifyingGlassIcon className={s.magniGlass}/> : ''}
+
             </div>
+
             {!!errorMessage && <Typography variant={'link2'}>{errorMessage}</Typography>}
+
         </div>
     )
 }
