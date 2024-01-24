a376b403e9cc2d4f9cf279d3c400220e1edcb433
Pasha <klonirovan89@gmail.com>
2024-01-24 16:08:46 +0300

    fixing checkboxControl component commit 3

--- src/components/ui/checkbox/checkbox.stories.tsx
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -1,61 +1,74 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type {Meta, StoryObj} from '@storybook/react'
 
-import { CheckboxControl } from './'
-import { useState } from "react";
+import {CheckboxControl} from './'
+import {useState} from "react";
 
 
 const meta = {
-  component: CheckboxControl,
-  tags: ['autodocs'],
-  title: 'Components/CheckboxControl',
+    component: CheckboxControl,
+    tags: ['autodocs'],
+    title: 'Components/CheckboxControl',
 } satisfies Meta<typeof CheckboxControl>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const OnlyCheckbox: Story = {
-  args: {
-    checked: false,
-    disabled: false,
-    variant: 'Checked',
-  },
-  render: args => {
-    const [checked, setChecked] = useState<boolean>(args.checked)
-    const onChangeCheckboxChecked = () => {
-      setChecked(!checked)
-    }
-    return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked} />
-  },
+    args: {
+        checked: false,
+        disabled: false,
+    },
+    render: args => {
+        const [checked, setChecked] = useState<boolean>(args.checked)
+        const onChangeCheckboxChecked = () => {
+            setChecked(!checked)
+        }
+        return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked}/>
+    },
 }
 
 export const Checked: Story = {
-  args: {
-    checked: true,
-    disabled: false,
-    label: 'Click me',
-    variant: 'Checked',
-  },
-  render: args => {
-    const [checked, setChecked] = useState<boolean>(args.checked)
-    const onChangeCheckboxChecked = () => {
-      setChecked(!checked)
-    }
-    return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked} />
-  },
+    args: {
+        checked: true,
+        disabled: false,
+        label: 'Click me',
+    },
+    render: args => {
+        const [checked, setChecked] = useState<boolean>(args.checked)
+        const onChangeCheckboxChecked = () => {
+            setChecked(!checked)
+        }
+        return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked}/>
+    },
 }
 
-export const Disabled: Story = {
-  args: {
-    checked: false,
-    disabled: true,
-    label: 'Click me',
-    variant: 'Disabled',
-  },
-  render: args => {
-    const [checked, setChecked] = useState<boolean>(args.checked)
-    const onChangeCheckboxChecked = () => {
-      setChecked(!checked)
-    }
-    return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked} />
-  },
+export const UnChecked: Story = {
+    args: {
+        checked: false,
+        disabled: false,
+        label: 'Click me',
+    },
+    render: args => {
+        const [checked, setChecked] = useState<boolean>(args.checked)
+        const onChangeCheckboxChecked = () => {
+            setChecked(!checked)
+        }
+        return <CheckboxControl {...args} onChange={onChangeCheckboxChecked} checked={checked}/>
+    },
+}
+
+export const DisabledChecked: Story = {
+    args: {
+        checked: true,
+        disabled: true,
+        label: 'Click me',
+    },
+}
+
+export const DisabledUnChecked: Story = {
+    args: {
+        checked: false,
+        disabled: true,
+        label: 'Click me',
+    },
 }
--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,27 +1,32 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import s from './checkbox.module.scss'
-import { CheckIcon } from '@radix-ui/react-icons';
-import { Typography } from "@/components/ui/typography";
+import {CheckIcon} from '@radix-ui/react-icons';
+import {Typography} from "@/components/ui/typography";
 
 export type CheckboxProps = {
-  onChange?: () => void
-  checked?: boolean
-  disabled?: boolean
-  label?: string
-  variant?: string
+    onChange: () => void
+    checked: boolean
+    disabled: boolean
+    label?: string
 }
 
 export const CheckboxControl = (props: CheckboxProps) => {
-  const { checked, disabled, label, onChange } = props
+    const {checked, disabled, label, onChange} = props
 
-  return (
-    <div className={s.checkbox}>
-      <Checkbox.Root className={s.CheckboxRoot} defaultChecked id="c1" checked={checked} disabled={disabled} onCheckedChange={onChange}>
-        <Checkbox.Indicator className={s.CheckboxIndicator}>
-          <CheckIcon />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
-      <Typography variant={'body2'}>{label}</Typography>
-    </div>
-  )
+    return (
+        <div className={s.div}>
+            <div className={disabled ? s.disabled : s.checkbox}>
+                <Checkbox.Root className={s.сheckboxRoot} defaultChecked id="c1" checked={checked} disabled={disabled}
+                               onCheckedChange={onChange}>
+                    <Checkbox.Indicator className={disabled ? s.сheckboxIndicatorDisabled : s.сheckboxIndicator}>
+                        <CheckIcon/>
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
+            </div>
+            <div className={disabled ? s.typographyDisabled : ''}>
+                {label && <Typography variant={'body2'}>{label}</Typography>}
+            </div>
+        </div>
+    )
 }
+
