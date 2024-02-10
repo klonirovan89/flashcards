66e4c1cd9858d9777fb6cb451d89caf9d8d3cb51
klonirovan89 <klonirovan89@gmail.com>
2024-02-10 15:04:44 +0300

    fixed Icon components

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -1,18 +1,16 @@
-import iconSprite from ".//src/components/ui/assets/icons-sprite.svg"
+import iconSprite from '@/assets/icons/icons-sprite.svg'
+
+export const Icon = (props: IconPropsType) => {
+    return (
+        <svg width={props.width} height={props.height}>
+            <use xlinkHref={`${iconSprite}#${props.iconId}`}/>
+        </svg>
+    );
+};
 
 type IconPropsType = {
     iconId: string
     width?: string
     height?: string
-    viewBox?: string
-    fill?: string
 }
-
-export const Icon = (props: IconPropsType) => {
-    return (
-        <svg width={props.width || "50"} height={props.height || "50"} viewBox={props.viewBox || "0 0 50 50"} fill="none" xmlns="http://www.w3.org/2000/svg">
-            <use xlinkHref={`${iconSprite}#${props.iconId}`}/>
-        </svg>
-    );
-};
 
