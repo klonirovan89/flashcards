6ea4bacb9d239e763be0c1607027daa63c880538
klonirovan89 <klonirovan89@gmail.com>
2024-02-11 13:10:02 +0300

    fixed Select component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,7 +1,7 @@
 import * as RadixSelect from "@radix-ui/react-select";
 import s from "../select/select.module.scss";
 import {Typography} from "@/components/ui/typography";
-import Layer from "@/components/ui/select/icon/Layer.svg";
+import {Icon} from "@/components/ui/icon/Icon";
 
 export const Select = (props: PropsType) => {
     const {
@@ -17,11 +17,13 @@ export const Select = (props: PropsType) => {
         <div className={s.container}>
             {label && <Typography variant={"body2"}>{label}</Typography>}
             <RadixSelect.Root defaultValue={selectedValue} onValueChange={(value) => handleSelectChange(value)}>
-                <RadixSelect.Trigger disabled={disabled}
-                                     className={classForPagination ? `${s.triggerPagination} ${s.trigger}` : s.trigger}>
-                    <RadixSelect.Value placeholder={selectedValue}/>
-                    <img className={s.img} src={Layer} alt="Layer"/>
-                </RadixSelect.Trigger>
+                    <RadixSelect.Trigger disabled={disabled}
+                                         className={classForPagination ? `${s.triggerPagination} ${s.trigger}` : s.trigger}>
+                        <RadixSelect.Value placeholder={selectedValue}/>
+                        <div className={s.arrow}>
+                            <Icon iconId={"Arrow"} width={"16px"} height={"16px"}/>
+                        </div>
+                    </RadixSelect.Trigger>
                 <RadixSelect.Portal>
                     <RadixSelect.Content position="popper" sideOffset={-1}>
                         <RadixSelect.Viewport
