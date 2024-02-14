ac02acc682c6a9dd546e3e1da545a18c1046e2da
Pavel Scherbachenka <klonirovan89@gmail.com>
2024-02-14 14:05:35 +0300

    fixed File Uploader with Icon component commit 1

--- src/components/ui/file-uploader-with-image-block/file-uploader-with-image.tsx
+++ src/components/ui/file-uploader-with-image-block/file-uploader-with-image.tsx
@@ -1,6 +1,7 @@
 import {useState, useRef} from 'react';
 import {SuperInput} from "../input/input";
 import {ButtonWithIcon} from "../button-with-icon/buttonWithIcon";
+import s from "./file-uploader-with-image.module.scss"
 
 export const FileUploaderWithImage = (props: PropsType) => {
     const {iconId, text} = props;
@@ -9,10 +10,27 @@ export const FileUploaderWithImage = (props: PropsType) => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [imageSrc, setImageSrc] = useState('');
 
+    const MAX_FILE_SIZE = 1048576; // 1MB in bytes
+    const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
+
+
+    console.log(selectedFile)
     const handleFileInputChange = (event: any) => {
         const file = event.target.files && event.target.files[0];
-        setSelectedFile(file);
 
+        if (file) {
+            if (!ALLOWED_FILE_TYPES.includes(file.type)) {
+                alert('Only image/jpeg, image/png, image/gif formats are supported.');
+                return;
+            }
+
+            if (file.size > MAX_FILE_SIZE) {
+                alert('Max image size is 1 MB.');
+                return;
+            }
+
+            setSelectedFile(file);
+        }
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
@@ -36,21 +54,25 @@ export const FileUploaderWithImage = (props: PropsType) => {
         setImageSrc('')
     };
 
-
-    console.log(selectedFile !== null ? selectedFile : 'deded')
-
     return (
-        <div >
-            <img src={imageSrc ? imageSrc : 'https://img.freepik.com/free-photo/red-prohibited-sign-no-icon-warning-or-stop-symbol-safety-danger-3d-illustration_56104-1991.jpg'}
-                 alt={'No photo'} style={{maxWidth: '100%', maxHeight: '100%'}}/>
+        <div className={s.container}>
+            <img className={s.img}
+                 src={imageSrc ? imageSrc : 'https://img.freepik.com/free-photo/red-prohibited-sign-no-icon-warning-or-stop-symbol-safety-danger-3d-illustration_56104-1991.jpg'}
+                 alt={'No photo'}/>
             <SuperInput
                 type="file"
                 ref={fileInputRef}
                 style={{display: 'none'}}
                 onChange={handleFileInputChange}
             />
-            <ButtonWithIcon onClick={handleButtonClick} iconId={iconId} text={text}/>
-            {imageSrc && <ButtonWithIcon onClick={deleteImageSrc} iconId={"Delete"} text={"Delete file"}/>}
+                {imageSrc ?
+                    <div className={s.buttonBlock}>
+                        <ButtonWithIcon onClick={handleButtonClick} iconId={iconId} text={text}/>
+                        <ButtonWithIcon onClick={deleteImageSrc} iconId={"Delete"} text={"Delete file"}/>
+                    </div>
+                    :
+                    <ButtonWithIcon className={s.buttonBlock} onClick={handleButtonClick} iconId={iconId} text={text} fullWidth={true}/>
+                }
         </div>
     );
 }
