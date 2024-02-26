ff1b4d47beb87964c38fed7ff255a6cb6b5b0ccb
Pasha <klonirovan89@gmail.com>
2024-02-26 18:42:38 +0300

    added base-api, router, decks and stpre components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-import { App } from '@/App'
+import { App } from './App'
 import ReactDOM from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
@@ -8,7 +6,5 @@ import '@fontsource/roboto/700.css'
 import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <App />
 )
---
+++ src/services/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {baseApi} from "@/services/base-api";
+
+export const store = configureStore({
+    reducer: {
+        [baseApi.reducerPath]: baseApi.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
