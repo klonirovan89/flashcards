5a58beb19310b6790228b71305fa864fe0ff8bda
klonirovan89 <klonirovan89@gmail.com>
2024-03-12 22:11:09 +0300

    fixed errors

--- src/components/ui/tab-switcher/tab-switcher.tsx
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -1,9 +1,8 @@
 import { Typography } from '@/components/ui/typography'
+import { ListValuesType } from '@/features/decks/filterControlDecks'
 import * as RadixTabsSwitcher from '@radix-ui/react-tabs'
 
 import s from './tab-switcher.module.scss'
-
-import { ListValuesType } from "@/features/decks/filterControlDecks"
 
 export const TabSwitcher = (props: PropsType) => {
   const { listValues, onChangeTabSwitcherValue, tabSwitcherValue, text } = props
